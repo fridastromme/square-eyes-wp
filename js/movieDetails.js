af8635c9a74add2bd7a0acd65e2ef63ee64e4af3
Fridastromme <fridastromme@hotmail.com>
2021-05-29 20:11:14 +0200

    Adjuster producer profile

--- js/movieDetails.js
+++ js/movieDetails.js
@@ -1,34 +1,41 @@
-const detailsContainer = document.querySelector("#watchmovie");
+const detailsContainer = document.querySelector ('#watchmovie');
 const queryString = document.location.search;
-const params = new URLSearchParams(queryString);
+const params = new URLSearchParams (queryString);
+const metaTitle = document.getElementsByTagName ('title')[0];
+const metaDescription = document.getElementsByTagName ('meta');
+const purchaseContainer = document.querySelector ('.purchase__container');
 
-const id = params.get("id");
-console.log(id);
+const id = params.get ('id');
+console.log (id);
 
-const fetchUrl = "https://mageknip.no/wp-json/wc/store/products/" + id;
+const fetchUrl = 'https://mageknip.no/wp-json/wc/store/products/' + id;
 
-async function fetchMovie() {
+async function fetchMovie () {
+  try {
+    const response = await fetch (fetchUrl);
+    const details = await response.json ();
 
-    try {
+    console.log (details);
 
-        const response = await fetch(fetchUrl);
-        const details = await response.json();
+    metaTitle.innerHTML = 'Square Eyes | Purchase «' + `${details.name}` + '»';
+    metaDescription.description.content =
+      'Purchase ' +
+      `${details.name}` +
+      ' ' +
+      `${details.short_description}` +
+      ' and start watching now!';
 
-        console.log(details);
-
-        detailsContainer.innerHTML =
-            `<img src="${details.images[0].src}" alt="${details.name}"/ class="details-image">
+    detailsContainer.innerHTML = `<img src="${details.images[0].src}" alt="${details.name}"/ class="details-image">
             <div class="details-text">
             <h1 class="details-title">${details.name}</h1>
             <h2 class="details-name">${details.short_description}</h2>
             <p class="details-description">${details.description}</p>
             <h3 class="details-price">Price: $${details.prices.price}
             </div>`;
-
-    } catch (error) {
-        console.log("Something went wrong when calling the API.")
-        detailsContainer.innerHTML = `<h1>We can't find the movie you're looking for.</h1>`;
-    }
+  } catch (error) {
+    console.log ('Something went wrong when calling the API.');
+    detailsContainer.innerHTML = `<h1>We can't find the movie you're looking for.</h1>`;
+  }
 }
 
-fetchMovie();
+fetchMovie ();
--- js/securityCode.js
+++ js/securityCode.js
@@ -1,29 +1,29 @@
-const cvv = document.querySelector("#cvv");
-const cvvError = document.querySelector("#cvvError");
-const button = document.querySelector(".submit-3");
+const cvv = document.querySelector ('#cvv');
+const cvvError = document.querySelector ('#cvvError');
+const button = document.querySelector ('.submit-3');
 
-function validateForm(event) {
-	event.preventDefault();
-	if (validateCvv(cvv.value)) {
-		cvvError.style.display = "none";
-		window.location.href = "/purchase-confirmation.html";
-	} else {
-		cvvError.style.display = "block";
-		console.log("Please enter 3 digits.")
-	}
+function validateForm (event) {
+  event.preventDefault ();
+  if (validateCvv (cvv.value)) {
+    cvvError.style.display = 'none';
+    window.location.href = '/purchase-confirmation.html';
+  } else {
+    cvvError.style.display = 'block';
+    console.log ('Please enter 3 digits.');
+  }
 }
-button.addEventListener("click", validateForm);
+button.addEventListener ('click', validateForm);
 
-function checkLength(value, len) {
-	if (value.trim().length > len) {
-		return true;
-	} else {
-		return false;
-	}
+function checkLength (value, len) {
+  if (value.trim ().length > len) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function validateCvv(cvv) {
-	const regEx = /^[0-9]{3}$/;
-	const cvvMatches = regEx.test(cvv);
-	return cvvMatches;
+function validateCvv (cvv) {
+  const regEx = /^[0-9]{3}$/;
+  const cvvMatches = regEx.test (cvv);
+  return cvvMatches;
 }
